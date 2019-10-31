4edf7ef50929cac3ee89e53c95b3fa5698ed0e6d
CodingCCarpenter <christineassists@aol.com>
2019-10-30 23:18:51 -0500

    add state to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
+import TodoList from './components/TodoList.js';
+import TodoForm from './components/TodoForm.js';
+import './styles.css';
+
+const  data = [
+  {
+    task: 'Sample Task',
+    id: 1528817077286,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //constructor with state
+  constructor() {
+    super();
+    this.state = {
+      todoItems: data
+    };
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  //class method that will be passed down to TodoList
+  toggleCompleted(taskId) {
+    console.log('toggleCompleted: ', taskId);
+
+    this.setState({
+      todoItems: this.state.data.map(task => {
+        if (task.id === taskId) {
+          return {
+            ...task, 
+            completed: !task.completed
+          };
+        }
+        return task;
+      })
+    });
+  }
+ 
   render() {
     return (
       <div>
